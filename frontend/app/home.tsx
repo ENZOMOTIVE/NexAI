207c2ea9f9593505a9f6719ce1a85d5d4e1ddae3
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2025-12-23 12:49:37 +0530

    fixed HomeScreen Render Glitch

--- frontend/app/home.tsx
+++ frontend/app/home.tsx
@@ -1,10 +1,28 @@
 import { View, Text, Button } from 'react-native';
 import { usePrivy } from '@privy-io/expo';
+import { router } from 'expo-router';
+import { useEffect } from 'react';
 
 export default function HomeScreen() {      
 
-    const {logout} = usePrivy();
-    const {user} = usePrivy();
+    const {logout,user} = usePrivy();
+
+
+    async function Logout(){
+      try {
+        await logout();
+        console.log('User logged out successfully');
+      } catch(e){
+        console.error('Logout failed', e);
+      }
+    }
+
+    useEffect(() => {
+      if(!user){
+        router.replace('/login')
+      }
+    }, [user]);
+    
     
 return (
 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -12,7 +30,7 @@ return (
   <View style={{ padding: 20 }}>
         <Text style={{ fontSize: 24 }}>Welcome to NexAI</Text>
         <Text>{JSON.stringify(user, null, 2)}</Text>
-        <Button title="Logout" onPress={logout} />
+        <Button title="Logout" onPress={Logout} />
       </View>
 </View>
 );
